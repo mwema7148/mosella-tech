749532282052f2511f65f151651311eb342350a2
mwema7148 <45680244+mwema7148@users.noreply.github.com>
2019-05-10 12:30:47 +0300

    Add files via upload

---
+++ helpers/Menu.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Menu Items
+ * All Project Menu
+ * @category  Menu List
+ */
+
+class Menu{
+	
+	
+	public static $navbarsideleft = array(
+		array(
+			'path' => 'home', 
+			'label' => 'Home', 
+			'icon' => '<i class="fa fa-home fa-3x"></i>'
+		),
+		
+		array(
+			'path' => 'sales/list', 
+			'label' => 'products', 
+			'icon' => '<i class="fa fa-product-hunt fa-3x"></i>'
+		),
+		
+		array(
+			'path' => 'services', 
+			'label' => 'Services', 
+			'icon' => '<i class="fa fa-wrench fa-3x"></i>'
+		),
+		
+		array(
+			'path' => 'contact_us/list', 
+			'label' => 'Contact us', 
+			'icon' => '<i class="fa fa-plug fa-3x"></i>'
+		),
+		
+		array(
+			'path' => 'about_us', 
+			'label' => 'About Us', 
+			'icon' => '<i class="fa fa-font fa-3x"></i>'
+		),
+		
+		array(
+			'path' => 'social_links/list', 
+			'label' => 'Social Links', 
+			'icon' => '<i class="fa fa-external-link fa-3x"></i>'
+		)
+	);
+
+	
+	
+}
